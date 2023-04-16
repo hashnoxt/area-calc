8247d38b33e0c9641b276de542c650ec78b0b570
Hashan Nalaka <hashannalaka0123@gmail.com>
2023-04-16 12:27:50 +0530

    F-04 C-02 : Test github actions
    F-04

--- app/test/CA2/test_CA2.py
+++ app/test/CA2/test_CA2.py
@@ -13,4 +13,4 @@ class TestCA2(unittest.TestCase):
 
     def test_rectangle_area(self):
         ca2 = CA2()
-        assert ca2.rectangle_area(a=A, b=B) == 8
+        assert ca2.rectangle_area(a=A, b=B) == 18
